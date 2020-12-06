be6131e271bf59cda32180dc58dae996fd45daef
milesbp <miles@paywithextend.com>
2020-12-06 02:46:03 -0500

    cleanup

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -2,6 +2,8 @@ import java.nio.file.Paths
 
 class Day5 {
 
+    private val seatMap = Array(128) { BooleanArray(8) }
+
     fun run1() {
         println("Day5 run 1")
         var highest = 0
@@ -17,22 +19,12 @@ class Day5 {
     }
 
     fun run2() {
-        println("Day5 run 1")
-        var highest = 0
-        processInput().forEach { rows ->
-            val row = convertToBinary(rows.substring(0, 7), 'F', 'B')
-            val col = convertToBinary(rows.substring(7), 'L', 'R')
-            if (row + col > highest) {
-                highest = row * 8 + col
-                println(row + col)
-            }
-            seatMap[row][col] = true
-        }
-
+        println("Day5 run 2")
+        setupSeatMap()
         for (i in seatMap.indices) {
             for (j in 1 until seatMap[i].size) {
                 if (!seatMap[i][j]) {
-                    println("empty seat at: row $i, col $j")
+                    //println("empty seat at: row $i, col $j")
                     if (seatMap[i][j - 1] && seatMap[i][j + 1]) {
                         println("My seat at: row $i, col $j\"")
                         println(i * 8 + j)
@@ -42,24 +34,24 @@ class Day5 {
         }
     }
 
-    private val seatMap = Array(128) { BooleanArray(8) }
+    private fun setupSeatMap() =
+        processInput().forEach { rows ->
+            val row = convertToBinary(rows.substring(0, 7), 'F', 'B')
+            val col = convertToBinary(rows.substring(7), 'L', 'R')
+            seatMap[row][col] = true
+        }
 
     fun processInput() =
         Paths.get(javaClass.classLoader.getResource("day5input.txt")!!.toURI()).toFile().useLines { it.toList() }
 
-    fun convertToBinary(input: String, zero: Char, one: Char): Int {
-        val builder = StringBuilder()
-        input.forEach { char ->
-            when (char) {
-                zero -> builder.append('0')
-                one -> builder.append('1')
-                else -> builder.append('0')
-
+    fun convertToBinary(input: String, zero: Char, one: Char): Int =
+        Integer.parseInt(StringBuilder().also { builder ->
+            input.forEach { char ->
+                when (char) {
+                    zero -> builder.append('0')
+                    one -> builder.append('1')
+                    else -> builder.append('0')
+                }
             }
-        }
-        return Integer.parseInt(builder.toString(), 2)
-    }
+        }.toString(), 2)
 }
-
-
-
